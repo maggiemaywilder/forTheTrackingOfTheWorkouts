c9702c7a917af87d0ebdcde9cad911a5e0b86966
Maggie Wilder <maggiemaywilder@gmail.com>
2021-04-08 09:05:37 -0500

    first routes

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const dbURI = `mongodb+srv://B3stpward:B3stpward@workoutdb.nkddx.mongodb.net/workoutDB?retryWrites=true&w=majority`;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true })
+    .then((result) => app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`)))
+    .catch((err) => console.log(err));
+
 
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const routes = require('./controllers/routes.js');
+app.use(require('./controllers/routes.js'));
 
-app.use(routes);
+    // example from class exercise
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
-app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
+
+
 
