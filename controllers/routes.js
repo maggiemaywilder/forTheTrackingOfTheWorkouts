6dd872d54902d5a06d8f70e3a552d54bb03afe2f
Maggie may <maggiemaywilder@gmail.com>
2021-04-13 21:23:58 -0500

    final simple routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,11 +1,56 @@
 const router = require('express').Router();
-const { Workout } = require('../models');
 const path = require('path');
 
-// // dashboard = /stats
-// router.get('/stats', (req, res) => {
+const { Workout } = require('../models');
+const { db } = require('../models/Workout');
 
-// });
+
+// load home
+router.get('/', (req, res) => {
+    res.sendFile('index.html')
+});
+
+// from home -> continue workout or new Workout
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+});
+
+// // from home -> click new workout
+router.post('/api/workouts', (req, res) => {
+    const workout = new Workout(req.body);
+    workout.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+});
+
+// add exercise button
+router.put('/api/workouts/:id', (req, res) => {
+    const exercise = req.body;
+    Workout.findById(req.params.id) 
+        .then((result) => {
+            result.exercises.push(exercise);
+            result.save();
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    });
+
+// dashboard = /stats
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+});
+
+// get workouts in range
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([])
+    console.log(req);
+});
+
 
 // get last workout --- should this have 
 router.get('/api/workouts', (req, res) => {
@@ -13,54 +58,33 @@ router.get('/api/workouts', (req, res) => {
         .then((data) => {res.json(data)})
 });
 
-// add exercise
-router.put('/api/workouts/:id', ( req, res) => {
-    const exercise = req.body;
-    const condition = `id = ${req.params.id}`;
-    // need id from params   _id: mongojs.ObjectId(params.id)
-    console.log(req.params.id, exercise, "line14-routes");
-
-    // Workout.findOne(condition, (error, res) => {
-    //     res.set()
-    //     res.save(function(error, document) {
-
-        // })
-    })
-// });
-
-// // create workout
-router.post('/api/workouts', (req, res) => {
-    const exercise = req.body;
-    console.log(exercise);
-    const workout = new Workout(exercise);
-    workout.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }); 
-
-// // get workouts in range
-// router.get('/api/workouts/range', (req, res) => {
-//     console.log(req)
-// });
-
-// // find or continue exercise
-// router.get('/exercise/:id', (req, res) => {
-//     console.log(req)
 // });
 
 
-// create exercise
-router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'))
-});
+// from Add your exercise -> add Exercise button -> handle form submit
+router.post('/api/workouts/:id', ( req, res) => {
 
-// load home
-router.get('/', (req, res) => {
-    res.sendFile('index.html')
+    console.log(req.body, req.params, "line14-routes");
+    if (req.params !== `id: 'undefined'`) {
+        const workout = new Workout({
+
+            exercises: [req.body]
+        });
+
+            workout.save()
+            .then((result) => {
+                res.send(result)
+            })
+            .catch((err) => {
+                console.log(err);
+            }); 
+
+    } else {
+        console.log('here, line33')
+        };    
+    // });
+    
+    
 });
 
 
--- models/Workout.js
+++ models/Workout.js
@@ -3,38 +3,40 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+var exerciseSchema = new Schema({
+    type: {
+        type: String,
+        required: true 
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    }
+}, { timestamps: true });
+
 var workoutSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now,
+        default: Date.now
     },
-    exercises: [{
-        type: {
-            type: String,
-            required: true 
-        },
-        name: {
-            type: String,
-            required: true
-        },
-        duration: {
-            type: Number,
-            required: true
-
-        },
-        weight: {
-            type: Number,
-        },
-        reps: {
-            type: Number,
-        },
-        sets: {
-            type: Number,
-        },
-        distance: {
-            type: Number,
-    }}],
-}, { timestamps: true });
+    exercises: [exerciseSchema]
+});
 
 const Workout = mongoose.model('Workout', workoutSchema);
 module.exports = Workout;
